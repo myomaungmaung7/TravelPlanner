31763840e8958abb4dee8147bba71f6a72852fa0
Yell Yint <yealyint1879@gmail.com>
2024-03-02 22:06:17 +0630

    AddNewDestination.js Created

---
+++ frontend/src/AddNewDestination.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddNewDestination() {
+  return (
+    <div>AddNewDestination</div>
+  )
+}
+
+export default AddNewDestination
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from './Login';
 import Register from './Register';
 import UnHome from './UnHome';
 import AdminProfile from './AdminProfile';
+import AddNewDestination from './AddNewDestination';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/login' element={<Login/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
           <Route path='/adminprofile' element={<AdminProfile/>}></Route>
+          <Route path='/addnewdestination' element={<AddNewDestination/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
